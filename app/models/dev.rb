f78a0986cb1c971a2dfdcb12e9e936d83be7d53e
Hannah Glazier <hrayglazier@aol.com>
2022-03-23 13:37:56 -0600

    Dev model done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,32 @@
 class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
+
+    def received_one(item_name) 
+        # self.freebies.map do |freebie|
+        #     self.freebie.item_name ? true : false
+        # end
+        # self.freebies.includes(:item_name).where('freebies.item_name = ?', "item_name")
+        # use where??
+        # existing_freebies = self.freebies.pluck(:item_name)
+        # existing_freebies == item_name ? true : false
+        # binding.pry
+        # self.freebies.include?(item_name) ? true : false
+
+        self.freebies.any? {|freebie| freebie.item_name == item_name}
+
+        end
+
+    def give_away(dev, freebie)
+        if self.freebies.include?(freebie)
+        Freebie.update(freebie.id, :dev => dev)
+
+        else 
+        Freebie.update(freebie.id, :dev => self)
+        end
+    end
+
+
+
+
 end
